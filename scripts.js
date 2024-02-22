5ef7471c9b120946557914da48f93df1d45dae12
Eduardo Morales <157326703+Eduardo-Renan-Morales@users.noreply.github.com>
2024-02-22 11:03:42 -0300

    add a funçao de conversão com js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,24 @@
 
 const convertButton = document.querySelector(".convert-Button")
 
-function convertValues(){
+function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyToConvert = document.querySelector(".currency-value-to-convert")      // valor em Real
+    const currencyToConverted = document.querySelector(".currency-value")               // Outras moedas
 
-    const dolarToday = 5.2
+    const dolarToday = 5.2                                                              // valor do dolar
 
-    const convertedValue = inputCurrencyValue / dolarToday
+    const convertedValue = inputCurrencyValue / dolarToday                              // moedas convertidas
 
-    console.log(convertedValue)
-}           
+    currencyToConvert.innerHTML = new Intl.NumberFormat("pt-br", {                       // valor digitado
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+    currencyToConverted.innerHTML = new Intl.NumberFormat("en-US", {                     // valores convertidos na tela
+        style: "currency",
+        currency: "USD"
+    }).format(convertedValue)
+}
 
 convertButton.addEventListener("click", convertValues)
